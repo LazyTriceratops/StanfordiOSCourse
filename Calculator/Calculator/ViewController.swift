aa844aeaa8a14de0e3b83bc8eeb0d40726f79219
Devin <devineror@gmail.com>
2016-04-23 11:37:29 -0700

    initial app setup

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Devin Eror on 4/22/16.
+//  Copyright © 2016 Devin Eror. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+}
+
